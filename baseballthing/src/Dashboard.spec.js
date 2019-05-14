3e6bd87fe98c48f024fb7a3e5e63e5e061e78b50
jmtblei <47536941+jmtblei@users.noreply.github.com>
2019-05-14 16:19:12 -0700

    display button tests passed

--- baseballthing/src/Dashboard.spec.js
+++ baseballthing/src/Dashboard.spec.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { render, fireEvent } from "react-testing-library";
+import 'react-testing-library/cleanup-after-each';
 
 import Dashboard from "./Dashboard.js";
 
--- baseballthing/src/Display.spec.js
+++ baseballthing/src/Display.spec.js
@@ -1,10 +1,22 @@
 import React from "react";
 import { render, fireEvent } from "react-testing-library";
+import 'react-testing-library/cleanup-after-each';
 
 import Display from "./Display.js";
 
 describe("<Display/>", () => {
   it("renders without crashing", () => {
     render(<Display />);
+  });
+
+  it("should render the numbers of strikes", () => {
+    const { getByText } = render(<Display />);
+    const strikeButton = getByText(/strikes/i);
+    fireEvent.click(strikeButton);
+  });
+  it("should render the numbers of balls", () => {
+    const { getByText } = render(<Display />);
+    const ballButton = getByText(/balls/i);
+    fireEvent.click(ballButton);
   });
 });
